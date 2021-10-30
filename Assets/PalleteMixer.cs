7b7f607e7d982eb8d2705d29d819b1d3250fd901
yukinyaa <yukinyaa712@gmail.com>
2021-10-30 23:39:40 +0900

    몬가 잘 안됨

--- Assets/PalleteMixer.cs
+++ Assets/PalleteMixer.cs
@@ -6,171 +6,274 @@ using System.Linq;
 using UnityEngine.EventSystems;
 
 using static StaticFunc;
+using Unity.Mathematics;
+using System;
+using UnityEngine.Profiling;
 
 [RequireComponent(typeof(RawImage))]
-public class PalleteMixer : MonoBehaviour, IPointerClickHandler
+public class PalleteMixer : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerExitHandler, IPointerDownHandler
 {
-    struct ColorBlob
-    {
-        public Color c;
-        public Vector2 place;
+    [SerializeField] float _superSampleRatio = 2;
+    RawImage _img;
+    
+
+    public float paintBlobSize = 300;
+    public float paintBlobAmount = 10;
+    public float paintSpreadSpeed = 0.1f;
+    public int PaintBlobSize => (int)(paintBlobSize * _superSampleRatio);
+    public float PaintBlobAmount => paintBlobAmount;
+    public int PaintBlobOutlinePixelCount => PaintBlobSize * 4 - 4;
+    public float AlphaMultiplier = 1;
+
+    RectTransform RT => transform as RectTransform;
+
+    int2  _sizeInPixel;
+    Vector2 _sizeInWorld;
+    Vector3 _bottomLeft, _topRight;
+
+    float[,] _colorWeight;
+    float[,] _colorWeightBuffer;
+    MColor[,] _colors;
+    MColor[,] _colorsBuffer;
+    Color[] _flattenedColors;
+    Texture2D _texture2D;
+
+    struct MColor 
+    { 
+        public float r, g, b, a;
+        public MColor(float r, float g, float b, float a)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+            this.a = a;
+        }
+        public static implicit operator Color(MColor col)
+        {
+            return new Color(col.r, col.g, col.b, col.a);
+        }
     }
-    List<ColorBlob> colorPlacements = new List<ColorBlob>();
-    RawImage img;
-    Texture2D texture2D;
 
-    public enum MixMode { Substractive, Additive, Mean }
-    [SerializeField] MixMode mixMode;
 
-    RectTransform rt => transform as RectTransform;
-    public float colorBlobSize = 300;
-
-    Vector2 sizeInPixel, sizeInWorld;
-    Vector3 bottomLeft, topRight;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        img = GetComponent<RawImage>();
+        _img = GetComponent<RawImage>();
 
-        sizeInPixel = GetScreenSizeInPixel(img.GetComponent<RectTransform>());
+        var sizeInPixelf = GetScreenSizeInPixel(_img.GetComponent<RectTransform>()) * _superSampleRatio;
+        _sizeInPixel = new int2((int)sizeInPixelf.x, (int)sizeInPixelf.y);
 
-        texture2D = new Texture2D((int)sizeInPixel.x, (int)sizeInPixel.y);
+        _texture2D = new Texture2D(_sizeInPixel.x, _sizeInPixel.y);
+        _colors = new MColor[_sizeInPixel.x, _sizeInPixel.y];
+        _colorWeight = new float[_sizeInPixel.x, _sizeInPixel.y];
 
+        _colorsBuffer = new MColor[_sizeInPixel.x, _sizeInPixel.y];
+        _colorWeightBuffer = new float[(int)_sizeInPixel.x, (int)_sizeInPixel.y];
+
+        _flattenedColors = new Color[_sizeInPixel.x * _sizeInPixel.y];
 
         Vector3[] corners = new Vector3[4];
-        rt.GetWorldCorners(corners);
+        RT.GetWorldCorners(corners);
 
-        RectTransformUtility.ScreenPointToWorldPointInRectangle(rt, RectTransformUtility.WorldToScreenPoint(Camera.main, corners[0]), Camera.main, out bottomLeft);
-        RectTransformUtility.ScreenPointToWorldPointInRectangle(rt, RectTransformUtility.WorldToScreenPoint(Camera.main, corners[2]), Camera.main, out topRight);
-        sizeInWorld = topRight - bottomLeft;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(RT, RectTransformUtility.WorldToScreenPoint(Camera.main, corners[0]), Camera.main, out _bottomLeft);
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(RT, RectTransformUtility.WorldToScreenPoint(Camera.main, corners[2]), Camera.main, out _topRight);
 
-        for (int x = 0; x < texture2D.width; x++)
-        {
-            for (int y = 0; y < texture2D.height; y++)
-            {
-                texture2D.SetPixel(x, y, new Color(1, 1, 1, 0));
-            }
-        }
-        texture2D.Apply();
-        img.texture = texture2D;
-
-        Debug.Log(sizeInPixel);
-        Debug.Log(sizeInWorld);
-
-
-    }
-
-    MixMode prevMixMode;
-
-    private void Update()
-    {
-        if (prevMixMode != mixMode)
-        {
-            prevMixMode = mixMode;
-            ForceUpdatePixels();
-        }
-    }
-
-    private void ForceUpdatePixels()
-    {
-        for (int x = 0; x < texture2D.width; x++)
-        {
-            for (int y = 0; y < texture2D.height; y++)
-            {
-                UpdatePixel(x, y);
-            }
-        }
-
-        texture2D.Apply();
-        img.texture = texture2D;
-    }
-
-    public void AddColor(Vector2 mousePos, Color color)
-    {
-        RectTransformUtility.ScreenPointToWorldPointInRectangle(rt, mousePos, Camera.main, out Vector3 localPos);
+        Debug.Log($"Dimension:{ _sizeInPixel }");
+        _sizeInWorld = _topRight - _bottomLeft;
         
-        Vector2 pixelPos = localPos - bottomLeft;
-        pixelPos.x *= sizeInPixel.x / sizeInWorld.x;
-        pixelPos.y *= sizeInPixel.y / sizeInWorld.y;
+        //for (int x = 0; x < _sizeInPixel.x; x++)
+        //{
+        //    for (int y = 0; y < _sizeInPixel.y; y++)
+        //    {
+        //        _texture2D.SetPixel(x, y, new MColor(1, 1, 1, 0));
+        //        _colorWeight[x, y] = 0;
+        //        _texture2DBuffer.SetPixel(x, y, new MColor(1, 1, 1, 0));
+        //        _colorWeightBuffer[x, y] = 0;
+        //    }
+        //}
+    }
 
+    
 
-        colorPlacements.Add(new ColorBlob { c = color, place = pixelPos });
-
-        for (int x = 0; x < texture2D.width; x++)
+    public void LateUpdate()
+    {
+        Profiler.BeginSample("Spread");
+        for (int y = 0; y < _sizeInPixel.y; y++)
         {
-            for (int y = 0; y < texture2D.height; y++)
+            for (int x = 0; x < _sizeInPixel.x; x++)
             {
-                Vector2 range = (Vector2)pixelPos - new Vector2(x, y);
-                if (range.sqrMagnitude < colorBlobSize && range.magnitude < colorBlobSize)
+                _colorsBuffer[x, y] = _colors[x, y];
+                _colorWeightBuffer[x, y] = 0;
+                float w;
+
+                if (_colorWeight[x, y] > 2)
                 {
-                    UpdatePixel(x, y);
+                    _colorWeightBuffer[x, y] = _colorWeight[x, y] * (1f - paintSpreadSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    _colorWeightBuffer[x, y] = _colorWeight[x, y];
+                }
+
+                for (int i = 0;i < 4;++i)
+                {
+                    int2 p;
+                    switch (i)
+                    {
+                        default:
+                        case 0:
+                            p = new int2(x, y + 1);
+                            break;
+                        case 1:
+                            p = new int2(x, y - 1);
+                            break;
+                        case 2:
+                            p = new int2(x + 1, y);
+                            break;
+                        case 3:
+                            p = new int2(x - 1, y);
+                            break;
+                    }
+                    if (IsPointInT2DRange(p) == false)
+                        continue;
+                    if (_colorWeightBuffer[p.x, p.y] > 2)
+                        MixColorToBuffer(_colorsBuffer[p.x, p.y], _colorWeightBuffer[p.x, p.y] * paintSpreadSpeed * Time.deltaTime, x, y);
+                    else
+                        MixColorToBuffer(_colorsBuffer[p.x, p.y], _colorWeightBuffer[p.x, p.y] * (1f - paintSpreadSpeed * Time.deltaTime), x, y);
+                }
+                if ((w = _colorWeightBuffer[x, y]) < AlphaMultiplier)
+                _colors[x, y].a = w / AlphaMultiplier;
+                _flattenedColors[x + y * _sizeInPixel.x] = _colorsBuffer[x, y];
+            }
+        }
+        Profiler.EndSample();
+
+
+
+        //commit
+        Swap(ref _colorsBuffer, ref _colors);
+        Swap(ref _colorWeightBuffer, ref _colorWeight);
+
+        Profiler.BeginSample("Apply");
+        _texture2D.SetPixels(_flattenedColors);
+        _texture2D.Apply();
+        _img.texture = _texture2D;
+        Profiler.EndSample();
+
+    }
+
+    void MixColorToBuffer(MColor c, float cw, int x, int y)
+    {
+        MColor oCol = _colorsBuffer[x, y];
+        float oW = _colorWeightBuffer[x, y];
+
+        MixColor(c, cw, oCol, oW, out oCol, out oW);
+
+        _colorWeightBuffer[x, y] = oW;
+        _colorsBuffer[x, y] = oCol;
+    }
+
+    public void AddColor(Vector2 mousePos, Color color_)
+    {
+        MColor color = new MColor(color_.r, color_.g, color_.b, color_.a);
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(RT, mousePos, Camera.main, out Vector3 localPos);
+
+        int2 pixelPos = MousePosToPixelpos(mousePos);
+
+        MColor mixedColor = new MColor();
+        float mixedColorw = 0;
+
+        int2 t2dDimension = _sizeInPixel;
+
+        for (int x = 0; x < t2dDimension.x; x++)
+        {
+            for (int y = 0; y < t2dDimension.y; y++)
+            {
+                int2 range = pixelPos - new int2(x, y);
+                float magnitude;
+                if (range.Max() < PaintBlobSize && (magnitude = range.Magnitude()) < PaintBlobSize)
+                {
+                    MixColor(mixedColor, mixedColorw, _colors[x, y], _colorWeight[x, y], out mixedColor, out mixedColorw);
+                    _colorWeight[x, y] = magnitude / PaintBlobSize * PaintBlobAmount;
+                    _colors[x, y] = color;
                 }
             }
         }
-        texture2D.Apply();
-        img.texture = texture2D;
+        foreach(int2 p in CircleOutlinePoints(pixelPos, PaintBlobSize))
+        {
+            if (IsPointInT2DRange(p) == false)
+                continue;
+            _colorWeight[p.x, p.y] = mixedColorw / PaintBlobOutlinePixelCount;
+            _colors[p.x, p.y] = mixedColor;
+        }
     }
+
+    public bool IsPointInT2DRange(int2 p)
+    {
+        return AABBPoint(new int2(0, 0), _sizeInPixel, p);
+    }
+
 
     public Color PickColor(Vector2 mousePos)
     {
-        RectTransformUtility.ScreenPointToWorldPointInRectangle(rt, mousePos, Camera.main, out Vector3 localPos);
-
-        Vector2 pixelPos = localPos - bottomLeft;
-        pixelPos.x *= sizeInPixel.x / sizeInWorld.x;
-        pixelPos.y *= sizeInPixel.y / sizeInWorld.y;
-
-        return texture2D.GetPixel((int)pixelPos.x, (int)pixelPos.y);
+        int2 pixelPos = MousePosToPixelpos(mousePos);
+        return _colors[pixelPos.x, pixelPos.y];
     }
 
-    void UpdatePixel(int x, int y)
+    public int2 MousePosToPixelpos(Vector2 mousePos)
     {
-        float r, g, b;
-        int cnt = 0;
-        r = g = b = 0;
-        Vector2 pos = new Vector2(x, y);
-
-        foreach (var colorBlob in colorPlacements)
-        {
-            Vector2 range = colorBlob.place - new Vector2(x, y);
-            
-            if (range.sqrMagnitude < colorBlobSize && range.magnitude < colorBlobSize)
-            {
-                cnt += 1;
-                
-                r += 1 - colorBlob.c.r;
-                g += 1 - colorBlob.c.g;
-                b += 1 - colorBlob.c.b;
-            }
-        }
-
-        if (cnt == 0)
-        {
-            texture2D.SetPixel(x, y, new Color(1, 1, 1, 0));
-            return;
-        }
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(RT, mousePos, Camera.main, out Vector3 localPos);
+        Vector2 pixelPos = localPos - _bottomLeft;
+        pixelPos.x *= _sizeInPixel.x / _sizeInWorld.x;
+        pixelPos.y *= _sizeInPixel.y / _sizeInWorld.y;
         
-
-        switch (mixMode)
-        {
-            case MixMode.Substractive:
-                Color col_s = new Color(1 - r, 1 - g, 1 - b);
-                texture2D.SetPixel(x, y, col_s);
-                break;
-            case MixMode.Additive:
-                Color col_a = new Color(cnt - r, cnt - g, cnt - b);
-                texture2D.SetPixel(x, y, col_a);
-                break;
-            case MixMode.Mean:
-                Color col_m = new Color(1 - r / cnt, 1 - g / cnt, 1 - b / cnt);
-                texture2D.SetPixel(x, y, col_m);
-                break;
-        }
-        
+        return new int2((int)pixelPos.x, (int)pixelPos.y);
     }
 
-    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    bool _isDragging = false;
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        _isDragging = true;
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        _isDragging = false;
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
     {
         FindObjectOfType<IndieStudio.DrawingAndColoring.Logic.GameManager>().SetToolColor(PickColor(eventData.position));
+    }
+
+
+    Vector2 _lastPixelPos;
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        FindObjectOfType<IndieStudio.DrawingAndColoring.Logic.GameManager>().SetToolColor(PickColor(eventData.position));
+        if (_isDragging)
+        {
+            Debug.Log("Draggin");
+        }
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        _isDragging = false;
+    }
+
+
+
+    static void MixColor(MColor a, float aw, MColor b, float bw, out MColor outc, out float outw)
+    {
+        outw = aw * bw;
+
+        outc.r = a.r * aw + b.r * bw / outw;
+        outc.g = a.g * aw + b.g * bw / outw;
+        outc.b = a.b * aw + b.b * bw / outw;
+
+        outc.a = 1;
     }
 }
--- Assets/StaticFunc.cs
+++ Assets/StaticFunc.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,5 +18,74 @@ public static class StaticFunc
         //Vector2 size = (v[2]) - (v[0]);
         return size;
     }
+    public static int SqrMagnitude(this int2 t)
+    {
+        return Math.Abs(t.x) + Math.Abs(t.y);
+    }
+    public static int Max(this int2 t)
+    {
+        return Math.Max(Math.Abs(t.x), Math.Abs(t.y));
+    }
+    public static float Magnitude(this int2 t)
+    {
+        return Mathf.Sqrt(t.x * t.x + t.y * t.y);
+    }
 
+    public static void Swap<T>(ref T a, ref T b)
+    {
+        T tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    public static bool AABBPoint(int2 minSqr, int2 maxSqr, int2 point)
+    {
+        if (point.x > minSqr.x || maxSqr.x > point.x) return false;
+        if (point.y > minSqr.y || maxSqr.y > point.y) return false;
+        return true;
+    }
+
+
+    static public IEnumerable<int2> FourD1Point(int2 p)
+    {
+        yield return new int2(p.x, p.y + 1);
+        yield return new int2(p.x, p.y - 1);
+        yield return new int2(p.x + 1, p.y);
+        yield return new int2(p.x - 1, p.y);
+    }
+    //http://groups.csail.mit.edu/graphics/classes/6.837/F98/Lecture6/circle.html
+    public static IEnumerable<int2> CircleOutlinePoints(int2 Center, int radius)
+    {
+        int x, y, r2;
+
+        r2 = radius * radius;
+        yield return new int2(Center.x, Center.y + radius);
+        yield return new int2(Center.x, Center.y - radius);
+        yield return new int2(Center.x + radius, Center.y);
+        yield return new int2(Center.x - radius, Center.y);
+
+        y = radius;
+        x = 1;
+        y = (int)(Mathf.Sqrt(r2 - 1) + 0.5);
+        while (x < y)
+        {
+            yield return new int2(Center.x + x, Center.y + y);
+            yield return new int2(Center.x + x, Center.y - y);
+            yield return new int2(Center.x - x, Center.y + y);
+            yield return new int2(Center.x - x, Center.y - y);
+            yield return new int2(Center.x + y, Center.y + x);
+            yield return new int2(Center.x + y, Center.y - x);
+            yield return new int2(Center.x - y, Center.y + x);
+            yield return new int2(Center.x - y, Center.y - x);
+            x += 1;
+            y = (int)(Math.Sqrt(r2 - x * x) + 0.5);
+        }
+        if (x == y)
+        {
+            yield return new int2(Center.x + x, Center.y + y);
+            yield return new int2(Center.x + x, Center.y - y);
+            yield return new int2(Center.x - x, Center.y + y);
+            yield return new int2(Center.x - x, Center.y - y);
+        }
+    }
 }
